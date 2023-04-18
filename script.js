1ff026d826bc303453d83f6cf85c82666a225965
Shehari931 <91692552+Shehari931@users.noreply.github.com>
2023-04-19 03:18:19 +0500

    adding the update file

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const nav_block = document.getElementById('nav_menu-block');
 
 const toggleNav = () => {
     nav_block.classList.toggle('d-none');
-    console.log('working')
 }
 
 for(i=0; i<btn.length; i++){
@@ -17,24 +16,51 @@ for(i=0; i<btn.length; i++){
 
 const header = document.getElementById('header');
 const headerdiv = document.getElementById('headerdiv');
-console.log(headerdiv.style.padding);
 
 const headerStyle = () => {
     if(window.scrollY > 50){
         header.style.background = '#fff';
         if(innerWidth > 992){
             headerdiv.style.padding = '32px 0px !important';
-            console.log(headerdiv.style.padding);
             headerdiv.setAttribute('style', 'padding: 32px 0px !important')
         }
     } else {
         header.style.background = 'transparent';
         if(innerWidth > 992){
             headerdiv.style.padding = '0px 0px';
-            console.log(headerdiv.style.padding);
             headerdiv.setAttribute('style', 'padding: 48px 0px !important')
         }
     }
 }
-
 window.addEventListener('scroll', headerStyle);
+
+
+// form submit button fuction 
+
+const submitBtn = document.getElementById('submitBtn');
+const emailInput = document.getElementById('email');
+const inputBlock = document.getElementById('inputBlock');
+const inputError = document.getElementById('inputError');
+
+
+
+const checkInput = () => {
+    const vaild = emailInput.checkValidity();
+    if(emailInput.value.length === 0) {
+        inputBlock.classList.add('border_invalid-input');
+        inputError.innerText = "Please Enter your email";
+    } else if(vaild) {
+        inputBlock.classList.remove('border_invalid-input');
+        inputError.innerText = "";
+        emailInput.value = "";
+    } else {
+        inputBlock.classList.add('border_invalid-input');
+        inputError.innerText = "Whoops, make sure it's an email";
+    }
+}
+
+
+submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    checkInput();
+})
